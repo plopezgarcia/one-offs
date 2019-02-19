678772c47d2c987c219808d5401786d2cd1b2ad6
Pablo Lopez-Garcia <pablo.lopez-garcia@cbmed.at>
2019-02-19 11:16:44 +0100

    database working

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,17 +1,22 @@
 # services/users/manage.py
 
-from flask.cli import FlaskGroup
-from project import app, db
 import unittest
 
+from flask.cli import FlaskGroup
 
-cli = FlaskGroup(app)
+from project import create_app, db   # new
+from project.api.models import User  # new
+
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
+
 
 @cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 @cli.command()
 def test():
@@ -21,6 +26,7 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
 
 if __name__ == '__main__':
     cli()
--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,36 +1,36 @@
 # services/users/project/__init__.py
 
+
 import os
-from flask import Flask, jsonify
+
+from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# model
-class User(db.Model):  # new
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+# new
+def create_app(script_info=None):
 
-#routes     
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
